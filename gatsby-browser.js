dafd3f5dc596f86bedf614c84579ec90bb81c8a6
Hunter Caron <huntercaron@mac.com>
2018-03-19 10:43:11 -0400

    tweak

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -51,7 +51,7 @@ const Navigation = styled.div`
     transform: translateY(${props => tabHeight*(props.numInMenu-1)}px);
 
     @media (-webkit-min-device-pixel-ratio: 2) {
-      transform: translateY(${props => tabHeight * (props.numInMenu + 1)}px);
+      transform: translateY(${props => tabHeight * (props.numInMenu + 1) - 44}px);
     }
   }
   .headroom--pinned {
@@ -82,7 +82,7 @@ const PseudoSection = styled.div`
   }
 
   @media (-webkit-min-device-pixel-ratio: 2) {
-    transform: translateY(${props => props.active ? 100 + "vh" : "calc(100vh - " + (83 + (tabHeight * (props.numInMenu - 1)) - props.vOffset)+44 + "px)"});
+    transform: translateY(${props => props.active ? 100 + "vh" : "calc(100vh - " + (77 + (tabHeight * (props.numInMenu - 1)) - props.vOffset)+44 + "px)"});
   }
 `
 
