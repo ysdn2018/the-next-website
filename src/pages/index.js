610837d998a23fb2396523ff14542853e4256942
Aaron Mohr <aaronjonmohr@gmail.com>
2018-04-04 14:11:03 -0400

    Home Page Statement

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
+import StatementHome from '../components/StatementHome'
 
 
 // styled components
@@ -9,16 +10,11 @@ const Container = styled.div`
 
 `
 
-const Subtitle = styled.div`
-  text-align: left;
-`
-
-
 // page component
 export default function IndexPage() {
   return (
     <Container>
-      <Subtitle>hey hows it goin? don't worry, we're workin on it. some kind of homepage will be here</Subtitle>
+      <StatementHome></StatementHome>
     </Container>
   )
 }
