3bef69f81ec4022aa2e80e507119f0b9bf225a60
Hunter Caron <huntercaron@me.com>
2020-01-30 10:07:27 +0100

    Updating packages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,128 +1,115 @@
 // /*
 //   this is where the pages are generated
 // */
-const path = require('path');
-const fs = require('fs');
-const { createFilePath } = require(`gatsby-source-filesystem`);
-
-const staticImagePath = "./static/assets/";
-const contentPath = "./src/content/";
-const imageNames = ["thumbnail", "image"];
-
+const path = require("path")
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const contentPath = "./src/content/"
+const imageNames = ["thumbnail", "image"]
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { frontmatter } = node;
-  
-  if (frontmatter) {
-    const images = [];
+    const { frontmatter } = node
 
-    for (let prop in frontmatter) {
-      if (imageNames.indexOf(prop) >= 0) {
-        images.push(prop);
-      }
-    }
-    
-    for (let imageName of images) {
-      let image = frontmatter[imageName];
+    if (frontmatter) {
+        const images = []
 
-      if (image) {
-        if (image.indexOf('/assets') === 0) {
-          frontmatter[imageName] = path.relative(
-            path.dirname(node.fileAbsolutePath),
-            path.join(__dirname, contentPath, image)
-          )
+        for (let prop in frontmatter) {
+            if (imageNames.indexOf(prop) >= 0) {
+                images.push(prop)
+            }
         }
-      }
+
+        for (let imageName of images) {
+            let image = frontmatter[imageName]
+
+            if (image) {
+                if (image.indexOf("/assets") === 0) {
+                    frontmatter[imageName] = path.relative(
+                        path.dirname(node.fileAbsolutePath),
+                        path.join(__dirname, contentPath, image)
+                    )
+                }
+            }
+        }
     }
-  }
 
-  // // adding images to static folder for preview (only in prod)
-  // if ((node.internal.mediaType == "image/png" || node.internal.mediaType === `image/jpeg`) && process.env.NODE_ENV === 'production') {
-  //   if (!fs.existsSync(staticImagePath)){
-  //     fs.mkdirSync(staticImagePath);
-  //   }
+    // adding slugs to pages
+    const { createNodeField } = actions
+    if (node.internal.type === `MarkdownRemark`) {
+        const slug = createFilePath({ node, getNode, basePath: `pages` })
 
-  //   fs.createReadStream(contentPath + node.relativePath).pipe(fs.createWriteStream(staticImagePath + node.base));
-  // }
+        const content = node.internal.content
+        const fixedPathContent = content.replace(/\(\/assets\//g, "(../assets/")
+        node.internal.content = fixedPathContent
 
-
-  // adding slugs to pages
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    
-    const content = node.internal.content;
-    const fixedPathContent = content.replace(/\(\/assets\//g, "(../assets/");
-    node.internal.content = fixedPathContent;
-    
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug,
+        })
+    }
 }
 
-
 exports.createPages = ({ actions, graphql }) => {
- const { createPage } = actions;
+    const { createPage } = actions
 
-  //allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
+    //allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
 
- return graphql(`
-   {
-     allMarkdownRemark(limit: 1000) {
-       edges {
-         node {
-           html
-           fields {
-             slug
-           }
-           frontmatter {
-             templateKey
-             title
-             category
-             category2
-             graduate
-           }
-         }
-       }
-     }
-   }
- `).then(result => {
-   if (result.errors) {
-     result.errors.forEach(e => console.error(e.toString()));
-     return Promise.reject(result.errors);
-   } 
-   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-     createPage({
-       path: node.fields.slug,
-       component: path.resolve(`src/templates/${String(node.frontmatter.templateKey)}.js`),
-       context: {
-         slug: node.fields.slug,
-         name: "/" + node.frontmatter.title + "/",
-         title: node.frontmatter.title,
-         graduateName: node.frontmatter.graduate,
-         graduate: "/" + node.frontmatter.graduate + "/",
-         category: "/" + node.frontmatter.category + "/",
-         category2: "/" + node.frontmatter.category2 + "/",
-       } // additional data can be passed via context
-     });
-   });
- });
-};
+    return graphql(`
+        {
+            allMarkdownRemark(limit: 1000) {
+                edges {
+                    node {
+                        html
+                        fields {
+                            slug
+                        }
+                        frontmatter {
+                            templateKey
+                            title
+                            category
+                            category2
+                            graduate
+                        }
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        if (result.errors) {
+            result.errors.forEach(e => console.error(e.toString()))
+            return Promise.reject(result.errors)
+        }
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            createPage({
+                path: node.fields.slug,
+                component: path.resolve(
+                    `src/templates/${String(node.frontmatter.templateKey)}.js`
+                ),
+                context: {
+                    slug: node.fields.slug,
+                    name: "/" + node.frontmatter.title + "/",
+                    title: node.frontmatter.title,
+                    graduateName: node.frontmatter.graduate,
+                    graduate: "/" + node.frontmatter.graduate + "/",
+                    category: "/" + node.frontmatter.category + "/",
+                    category2: "/" + node.frontmatter.category2 + "/",
+                }, // additional data can be passed via context
+            })
+        })
+    })
+}
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /intersection-observer/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
+    if (stage === "build-html") {
+        actions.setWebpackConfig({
+            module: {
+                rules: [
+                    {
+                        test: /intersection-observer/,
+                        use: loaders.null(),
+                    },
+                ],
+            },
+        })
+    }
 }
